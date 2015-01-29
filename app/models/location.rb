d4c2b28f0f553e8263d1c632de909b7894b07831
andygimma <andy.n.gimma@gmail.com>
2015-01-29 02:01:02 -0500

    adjust geocoded address

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,15 +1,19 @@
 class Location < ActiveRecord::Base
 
-  geocoded_by :full_street_address   # can also be an IP address
+  geocoded_by :geocodable_address   # can also be an IP address
   after_validation :geocode
 
   belongs_to :region
   has_many :workshops
-  
+
   validates :name, presence: true
   validates :full_street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
   validates :region_id, presence: true
+
+  def geocodable_address
+    "#{full_street_address} #{city} #{state} #{zip_code}"
+  end
 end
