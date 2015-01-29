cbbb1c43dc47f28ffd02655451c9874f0a56e98c
andygimma <andy.n.gimma@gmail.com>
2015-01-29 01:52:41 -0500

    some design tweaks

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @workshop_dates = WorkshopDate.where(status: "approved").order(start_date: :desc).limit(5)
-    @workshops = Workshop.where(status: "approved", user_id: current_user.id)
+    @workshops = Workshop.where(user_id: current_user.id)
   end
 end
